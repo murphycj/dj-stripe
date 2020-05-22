0e446169d049b4547130b9d3dad88ea1cfa21cd6
Charlie Murphy, PhD <murphy.charlesj@gmail.com>
2020-05-22 16:34:01 -0400

    subscription.start can be null and blank

--- djstripe/models/billing.py
+++ djstripe/models/billing.py
@@ -1391,6 +1391,8 @@ class Subscription(StripeModel):
     # TODO: schedule (implement model SubscriptionSchedule, see #899)
     # .start is deprecated - will be removed in 2.4 - use .start_date instead
     start = StripeDateTimeField(
+        null=True,
+        blank=True,
         help_text="Date of the last substantial change to "
         "this subscription. For example, a change to the items array, or a change "
         "of status, will reset this timestamp."
